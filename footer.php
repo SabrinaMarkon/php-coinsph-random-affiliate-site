eb716aa421fe20887f9d81f902efab9fbb3ff24e
Sabrina Markon <phpsitescripts@outlook.com>
2018-11-11 03:04:53 -0700

    Apply Bootstrap 3 template to base php script framework. Design template for ad card row.

--- footer.php
+++ footer.php
@@ -1,16 +1,66 @@
 
+		</div>
+			</div>
+	  <!-- /.row -->
+	  
+
+	<div class="centered">
+
+		<section class="cards">
+
+			<article class="card">
+				<div class="text-center"><img class="card-image" alt="" /></div>
+				<p>content for ad one</p>
+			</article><!-- /card-one -->
+
+			<article class="card">
+				<div class="text-center"><img class="card-image" alt="" /></div>
+				<p>content for ad two</p>
+			</article><!-- /card-two -->
+
+			<article class="card">
+				<div class="text-center"><img class="card-image" alt="" /></div>
+				<p>content for ad three</p>
+			</article><!-- /card-three -->
+
+			<article class="card">
+				<div class="text-center"><img class="card-image" alt="" /></div>
+				<p>content for ad four</p>
+			</article><!-- /card-four -->
+
+			<article class="card">
+				<div class="text-center"><img class="card-image" alt="" /></div>
+				<p>content for ad five</p>
+			</article><!-- /card-four -->
+
+			<article class="card">
+				<div class="text-center"><img class="card-image" alt="" /></div>
+				<p>content for ad six</p>
+			</article><!-- /card-four -->
+			
+		</section>
+
 	</div>
+      <!-- /.row -->
 
-<footer class="footer" id="ja-footer">
-<p>&copy;2018 RandomBTCAds.com. All rights reserved.</p>
-<p>Made with <i class="fa fa-heart" aria-hidden="true" style="color:#be1931"></i></p>
-</footer>
+    </div>
+    <!-- /.container -->
 
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">
+		&copy;2018 RandomBTCAds.com. All rights reserved. 
+		<br />Made with <i class="fa fa-heart" aria-hidden="true" style="color:#be1931"></i> by <a href="http://sabrinamarkon.com" target="_blank">SabrinaMarkon.com</a></p>
+		</p>
+      </div>
+      <!-- /.container -->
+    </footer>
 
-</div><!-- main container -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="js/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+  </body>
 
-<script src="//code.jquery.com/jquery.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-</body>
 </html>
--- header.php
+++ header.php
@@ -1,51 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Junior Artists</title>
-<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-<link href="/css/custom.css" rel="stylesheet" media="screen">
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-<body>
 
-<div class="container text-center">
+  <head>
 
-	<header id="heading">
-		<img src="/images/header.jpg" alt="Random BTC Ads" id="ja-headerimage">
-	</header>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Sabrina Markon">
 
-	<div class="btn-group btn-group-justified ja-navgroup" role="group" aria-label="Main Navigation Menu" id="navbar">
+    <title>RandomBTCAds.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="css/custom.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Random BTC Ads</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+
+			<?php
+			if ((isset($_SESSION['username'])) && (isset($_SESSION['password'])))
+			{
+			?>
+            <li class="nav-item active">
+              <a class="nav-link" href="/members">Main
+                <span class="sr-only">(current)</span>
+              </a>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="/profile">Profile</a>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="/earnings">Earnings</a>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="/ads">Ads</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/logout">Contact</a>
+			</li>
+			<?php
+			}
+			else
+			{
+			?>
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home
+                <span class="sr-only">(current)</span>
+              </a>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="/register">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+			</li>
+			<?php
+			}
+			?>
+					
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header with Background Image -->
+    <header class="ja-headerimage">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <h1 class="display-3 text-center text-white mt-4">Random BTC Ads</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+		<div class="row">
+			<div class="col-sm-12">		
+
+	asdfadsfdsfdsffds		
 		
-		<?php
-		if ((isset($_SESSION['username'])) && (isset($_SESSION['password'])))
-		{
-		?>
-		<a href="/members" type="button" class="btn ja-navbutton" role="button">MAIN</a>
-		<a href="/profile" type="button" class="btn ja-navbutton" role="button">PROFILE</a>
-		<a href="/post" type="button" class="btn ja-navbutton" role="button">POST</a>
-		<a href="/contact" type="button" class="btn ja-navbutton" role="button">CONTACT</a>
-		<a href="/logout" type="button" class="btn ja-navbutton" role="button">LOGOUT</a>
-		<?php
-		}
-		else
-		{
-		?>
-		<a href="/" type="button" class="btn ja-navbutton" role="button">HOME</a>
-		<a href="/login" type="button" class="btn ja-navbutton" role="button">LOGIN</a>
-		<a href="/register" type="button" class="btn ja-navbutton" role="button">REGISTER</a>
-		<a href="/aboutus" type="button" class="btn ja-navbutton" role="button">ABOUT</a>
-		<a href="/contact" type="button" class="btn ja-navbutton" role="button">CONTACT</a>
-		<?php
-		}
-		?>
-	</div>
 
-	<div class="ja-content">
