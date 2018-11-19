d25c236cadba8a6a9d76ce1788847133e35b2b4c
Sabrina Markon <phpsitescripts@outlook.com>
2018-11-19 01:50:24 -0700

    Secure main files and member area from direct access

--- control.php
+++ control.php
@@ -1,4 +1,10 @@
 <?php
+# Prevent direct access to this file. Show browser's default 404 error instead.
+if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit;
+}
+
 if ((isset($_SESSION['username'])) && (isset($_SESSION['password'])))
 {
 $logincheck = new User();
